ffd1b5582edc2559d8c9d83dc0ad603c3b3d5e20
Carlos Cofré <carlos.cofre@usach.cl>
2020-06-03 15:17:59 -0400

    Se consiguen todos los filtros

--- testJpeg.c
+++ testJpeg.c
@@ -247,17 +247,19 @@ bool write_jpeg_bn(const JpegData *jpegData,
 int main(void){
 
 
-    JpegData jpegDataOri, jpegDataDst, jpegDataDstBn;
-    
+    JpegData jpegDataOri, jpegDataDst, jpegDataDstBn, jpegDataDstBnLap, jpegDataDstBnLapUm;
 
     // 
     struct jpeg_compress_struct cinfo;
     struct jpeg_error_mgr jerr;
+    int UMBRAL = 100;
 
     // src/dst file
     char *src = "./img_test/lena_.jpg";
     char *dst = "./img_test/out.jpg";
     char *dstBn = "./img_test/outBn.jpg";
+    char *dstBnLap = "./img_test/outBnLap.jpg";
+    char *dstBnLapUm = "./img_test/outBnLapUm.jpg";
 
     if (!read_jpeg(&jpegDataOri, src, &jerr)){
         free_jpeg(&jpegDataOri);
@@ -271,25 +273,52 @@ int main(void){
         return -1;
     }
 
-    //printf("Read:  %s\n", src);
-
     if (!read_jpeg(&jpegDataDstBn, src, &jerr)){
         free_jpeg(&jpegDataDstBn);
         return -1;
     }
     jpegDataDstBn.ch = 1; // se fuerza 1 canal en el destino BN
 
+    if (!read_jpeg(&jpegDataDstBnLap, src, &jerr)){
+        free_jpeg(&jpegDataDstBnLap);
+        return -1;
+    }
+    jpegDataDstBnLap.ch = 1; // se fuerza 1 canal en el destino BN
+
+    if (!read_jpeg(&jpegDataDstBnLapUm, src, &jerr)){
+        free_jpeg(&jpegDataDstBnLapUm);
+        return -1;
+    }
+    jpegDataDstBnLapUm.ch = 1; // se fuerza 1 canal en el destino BN
+
     //printf("Read:  %s\n", src);
 
-
+    // se propone una imagen guardada en un solo arreglo
+    // así, una imagen de dimensiones m*n (ancho x alto) que se ve
+    // 0 1 2 3 .... m
+    // n+1 n+2 .... 2m
+    // .
+    // .
+    // ............ m*n
+    // queda representada de la siguiente forma
+    // 0, 1, 2, 3, ... m+1, m+2, ... m*n
+    
     // reverse all bits
     int sizeOri = jpegDataOri.width * jpegDataOri.height * jpegDataOri.ch;
-    float arr[]={.3,.59,.11};
+    float arr[]={.3,.59,.11};   // factores para conversion a BN
+    int lapMasc[3][3] = {   {1, 0, 1},
+                            {0,-4, 0},
+                            {1, 0, 1} };
+
 
     //float val = 0.0;
     float val;
     int suma = 0, cont=0;
-    for (int i=0; i<sizeOri; i+=jpegDataOri.ch) {
+    int i, j, ii, jj;
+    int alto=0, ancho=0;
+    int pixelXY = 0, pixelXYLap = 0;
+
+    for (i=0; i<sizeOri; i+=jpegDataOri.ch) {
 
         jpegDataDst.data[i]= jpegDataOri.data[i] ;
 
@@ -299,7 +328,9 @@ int main(void){
         }
     }
 
+    
     // Escritura
+    /*
     if (!write_jpeg(&jpegDataDst, dst, &jerr, JCS_RGB)){
         free_jpeg(&jpegDataDst);
         return -1;
@@ -307,41 +338,70 @@ int main(void){
 
     printf("Write: %s\n", dst);
     free_jpeg(&jpegDataDst);
-
+    */
 
     // Escritura bn
     if (!write_jpeg(&jpegDataDstBn, dstBn, &jerr, JCS_GRAYSCALE)){
         free_jpeg(&jpegDataDstBn);
         return -1;
     }
+    //printf("Write: %s\n", dstBn);
+    //free_jpeg(&jpegDataDstBn);
 
+    // recorrido imagen para laplaciano
+    alto = jpegDataDstBn.height;
+    ancho = jpegDataDstBn.width;
+
+
+    for (j=1; j<alto-1; j++){     // filas desde la segunda (indice 1) hasta la penultima (indice n-1)
+        for(i=1; i<ancho-1; i++){ // columnas desde la segunda (indice 1) hasta la penultima (indice m-1)
+            
+            for(jj=-1; jj<=1;jj++){
+                for(ii=-1; ii<=1; ii++){
+                    pixelXY += lapMasc[jj+1][ii+1] * jpegDataDstBn.data[(i+ii)+ancho*(j+jj)];
+                }
+            }
+            jpegDataDstBnLap.data[i+ancho*j] = pixelXY;
+            pixelXY = 0;
+        }
+        //printf("\n");
+    }
+
+    // umbral
+    for(i=0; i<ancho*alto; i++){
+        jpegDataDstBnLapUm.data[i] = jpegDataDstBnLap.data[i]>UMBRAL?255:0;
+    }
+
+
+    ///////////////////////////////     ESCRITURAS
+
+    // Escritura bn
+    if (!write_jpeg(&jpegDataDstBn, dstBn, &jerr, JCS_GRAYSCALE)){
+        free_jpeg(&jpegDataDstBn);
+        return -1;
+    }
     printf("Write: %s\n", dstBn);
     free_jpeg(&jpegDataDstBn);
+
+
+    // Escritura bn-laplaciano
+    if (!write_jpeg(&jpegDataDstBnLap, dstBnLap, &jerr, JCS_GRAYSCALE)){
+        free_jpeg(&jpegDataDstBnLap);
+        return -1;
+    }
+    printf("Write: %s\n", dstBnLap);
+    free_jpeg(&jpegDataDstBnLap);
+    
+
+    // Escritura bn-laplaciano-umbral
+    if (!write_jpeg(&jpegDataDstBnLapUm, dstBnLapUm, &jerr, JCS_GRAYSCALE)){
+        free_jpeg(&jpegDataDstBnLapUm);
+        return -1;
+    }
+    printf("Write: %s\n", dstBnLapUm);
+    free_jpeg(&jpegDataDstBnLapUm);
        
-    
-
-    
-    /*
-    for(int y=0; y<jpegData.height; y++){
-      for(int x=0; x<jpegData.width; x++){
-        for(int ch=0; ch<jpegData.ch; ch++){
-          //printf("%03d, ", jpegData.data[y*jpegData.width+x+ch]);
-          val += arr[ch]*(float)(jpegData.data[y*jpegData.width+x+ch]);
-          //printf("%03d, ", jpegData.data[y*jpegData.width+x*3+ch]);
-          //printf("%3d ", y*jpegData.height+x*jpegData.ch+ch);
-          //printf("%.1f,", val);
-          }
-        //printf("%3d ", y*jpegData.height+x);
-        printf("%c ", val>100.0?'_':'#');
-        //printf("%s",val<100.0?" # ":" _ ");
-        //printf("%3d ", (int)val);
-        val = 0.0;
-        //printf(" ");
-      }
-      printf("\n");
-    }*/
-
-    
+        
 
     return 0;
 }
